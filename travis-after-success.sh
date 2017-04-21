c22b0001ce25ebbb99ee14a34df123f2f29252ca
Sunjay Varma <sunjay@users.noreply.github.com>
2017-04-21 00:22:30 -0400

    Travis Fixes (#12)
    * Since travis commit isn't working, trying travis branch environment variable
    
    * Made sure the build doesn't fail if cargo-benchcmp is already installed
    
    * Added code coverage kcov
    
    * Fixing .travis.yml doc command
    
    * Making sure cargo-travis install doesn't fail the build
    
    * Making sure cargo-travis install doesn't fail the build (try #2)
    
    * Meant to use ; instead of ||
    
    * Changing travis script
    
    * Trying to cache kcov
    
    * Fixing cache configuration on travis
    
    * Replacing test command with coverage command and trying the older cache configuration
    
    * Caching kcov directory
    
    * Trying to cache kcov did not work out
    
    * caching just kcov/build
    
    * Ignoring the exit code of cargo coverage
    
    * Removed bencher dependency that is no longer used
    
    * Verbosely outputting benchmark
    
    * Moving cargo coverage back to after success because it eats up too much build time

--- travis-after-success.sh
+++ travis-after-success.sh
@@ -5,6 +5,7 @@ set -x
 
 if [ "${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}" != "master" ] && [ "$TRAVIS_RUST_VERSION" == "nightly" ]; then
     REMOTE_URL="$(git config --get remote.origin.url)";
+    cargo install cargo-benchcmp;
     # Clone the repository fresh..for some reason checking out master fails
     # from a normal PR build's provided directory
     cd ${TRAVIS_BUILD_DIR}/.. && \
@@ -12,10 +13,9 @@ if [ "${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}" != "master" ] && [ "$TRAVIS
     cd  "${TRAVIS_REPO_SLUG}-bench" && \
     # Bench the pull request base or master
     git checkout "${TRAVIS_PULL_REQUEST_BRANCH:-master}" && \
-    cargo bench | tee previous-benchmark && \
+    cargo bench --verbose | tee previous-benchmark && \
     # Bench the current commit that was pushed
-    git checkout ${TRAVIS_COMMIT} && \
-    cargo bench | tee current-benchmark && \
-    cargo install cargo-benchcmp --force && \
+    git checkout ${TRAVIS_BRANCH} && \
+    cargo bench --verbose | tee current-benchmark && \
     cargo benchcmp previous-benchmark current-benchmark;
 fi
